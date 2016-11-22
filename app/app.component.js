38717e85e1bbbb22418c5bfa419ab241360bd9ba
sajan <sjnmishra8@gmail.com>
2016-11-23 02:32:37 +0530

    finished chapter 5 - services

--- app/app.component.js
+++ app/app.component.js
@@ -9,23 +9,19 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var HEROES = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-];
+var hero_service_1 = require('./hero.service');
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(heroService) {
+        this.heroService = heroService;
         this.title = 'Tour of heroes';
-        this.heroes = HEROES;
     }
+    AppComponent.prototype.getHeroes = function () {
+        var _this = this;
+        this.heroService.getHeroes().then(function (heroes) { return _this.heroes = heroes; });
+    };
+    AppComponent.prototype.ngOnInit = function () {
+        this.getHeroes();
+    };
     AppComponent.prototype.onSelect = function (hero) {
         this.selectedHero = hero;
     };
@@ -33,9 +29,10 @@ var AppComponent = (function () {
         core_1.Component({
             selector: 'my-app',
             template: "\n    <h1>{{title}}</h1>\n    <h2>My Heroes</h2>\n    <ul class=\"heroes\">\n      <li *ngFor=\"let hero of heroes\" (click)=\"onSelect(hero)\" [class.selected]=\"hero === selectedHero\">\n        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n      </li>\n    </ul>\n\t\t<my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n  ",
-            styles: ["\n  .selected {\n    background-color: #CFD8DC !important;\n    color: white;\n  }\n  .heroes {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 15em;\n  }\n  .heroes li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .heroes li.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n  }\n  .heroes li:hover {\n    color: #607D8B;\n    background-color: #DDD;\n    left: .1em;\n  }\n  .heroes .text {\n    position: relative;\n    top: -3px;\n  }\n  .heroes .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0 0 4px;\n  }\n"]
+            styles: ["\n  \t.selected {\n    \tbackground-color: #CFD8DC !important;\n    \tcolor: white;\n\t\t}\n  \t.heroes {\n    \tmargin: 0 0 2em 0;\n    \tlist-style-type: none;\n    \tpadding: 0;\n    \twidth: 15em;\n  \t}\n  \t.heroes li {\n    \tcursor: pointer;\n    \tposition: relative;\n    \tleft: 0;\n    \tbackground-color: #EEE;\n    \tmargin: .5em;\n    \tpadding: .3em 0;\n    \theight: 1.6em;\n    \tborder-radius: 4px;\n  \t}\n  \t.heroes li.selected:hover {\n    \tbackground-color: #BBD8DC !important;\n    \tcolor: white;\n  \t}\n  \t.heroes li:hover {\n    \tcolor: #607D8B;\n    \tbackground-color: #DDD;\n    \tleft: .1em;\n  \t}\n  \t.heroes .text {\n    \tposition: relative;\n    \ttop: -3px;\n\t\t}\n  \t.heroes .badge {\n    \tdisplay: inline-block;\n    \tfont-size: small;\n    \tcolor: white;\n    \tpadding: 0.8em 0.7em 0 0.7em;\n    \tbackground-color: #607D8B;\n    \tline-height: 1em;\n    \tposition: relative;\n    \tleft: -1px;\n    \ttop: -4px;\n    \theight: 1.8em;\n    \tmargin-right: .8em;\n    \tborder-radius: 4px 0 0 4px;\n\t\t}\n\t"],
+            providers: [hero_service_1.HeroService]
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [hero_service_1.HeroService])
     ], AppComponent);
     return AppComponent;
 }());
